ed25e809cf4d2d036870cd04dc2a46d1403f3baa
Hussain Abbas <hussain@hussainabbas.com>
2016-02-27 17:50:44 -0500

    testing...

--- pull4me.sh
+++ pull4me.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+read -p "Commit description: " desc
+git checkout master
+git add -A && \
+git commit -am "$desc" && \
+git push
 ssh myspacethenet@myspacethenet.cloudapp.net 'bash -s' < ./pull4me-scripts.sh
-
-
